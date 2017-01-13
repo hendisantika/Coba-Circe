1270f9b20e098ca21342ad65f98d6aed381b46e4
hendisantika <hendisantika@gmail.com>
2017-01-13 07:29:21 +0700

    Serialization

--- src/main/scala/com/hendisantika/circe/Serialization.scala
+++ src/main/scala/com/hendisantika/circe/Serialization.scala
@@ -3,6 +3,21 @@ package com.hendisantika.circe
 /**
   * Created by hendisantika on 12/01/17.
   */
-class Serialization {
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
 
+//@SerialVersionUID(15L)
+//class Animal(name: String, age: Int) extends Serializable {
+//  override def toString = s"Animal($name, $age)"
+//}
+
+//case class Person(name: String) extends Serializable
+
+object MySerialize extends App {
+  val fos = new FileOutputStream("../a.tmp")
+  val oos = new ObjectOutputStream(fos)
+
+  oos.writeObject(new Animal("Dvorak", 12))
+  oos.writeObject(Person("Dijkstra"))
+  oos.close
 }
