32363b5ff4c5db228a30bc9c5ff4b138d83d798f
hendisantika <hendisantika@gmail.com>
2017-01-13 07:22:50 +0700

    Xtract Json Value

--- src/main/scala/com/hendisantika/circe/XtractJSON.scala
+++ src/main/scala/com/hendisantika/circe/XtractJSON.scala
@@ -8,37 +8,7 @@ import io.circe.optics.JsonPath._
 /**
   * Created by hendisantika on 09/01/17.
   */
-object CobaCirce4 extends App {
-  //  val json: Json = parse("""
-  //  {
-  //    "responseHeader":{
-  //      "status":0,
-  //      "QTime":1,
-  //      "params":{
-  //      "indent":"true",
-  //      "q":"content_t:ibadah &&\narticle_id_l:6729941",
-  //      "wt":"json"}},
-  //    "response":{"numFound":1,"start":0,"docs":[
-  //    {
-  //      "id":"comment:9404033",
-  //      "comment_id_l":9404033,
-  //      "article_id_l":6729941,
-  //      "reply_to_id_l":0,
-  //      "user_id_l":1686628,
-  //      "username_s":"Maukayakerja",
-  //      "email_s":"",
-  //      "content_t":"Rajin ibadah.",
-  //      "report_i":0,
-  //      "type_s":"comment",
-  //      "category_is":[71],
-  //      "category_ss":["Politik"],
-  //      "created_dt":"2016-05-19T02:12:01Z",
-  //      "created_ts_l":1463623921,
-  //      "is_deleted_i":0,
-  //      "country_code_s":"id",
-  //      "_version_":1534721405555310592}]
-  //    }}""").getOrElse(Json.Null)
-
+object XtractJSON extends App {
   val json: Json = parse(
     """
     {
