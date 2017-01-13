9d0edd5e592df9f925fb49e16eba88643f76f708
hendisantika <hendisantika@gmail.com>
2017-01-13 08:44:48 +0700

    Bug Fix Class2JSON.scala

--- src/main/scala/com/hendisantika/circe/Class2JSON.scala
+++ src/main/scala/com/hendisantika/circe/Class2JSON.scala
@@ -1,11 +1,22 @@
 package com.hendisantika.circe
 
+import io.circe.generic.auto._
+import io.circe.syntax._
+
 /**
   * Created by hendisantika on 13/01/17.
   */
 
-@JsonCodec case class Bar(i: Int, s: String)
+/*@JsonCodec */case class Shinobi(i: Int, s: String)
 
-object Class2JSON {
+object Class2JSON extends App{
+  val a = new Shinobi(13, "Uzumaki Naruto")
+  val shinobi = a
+  println(s"Asalnya --> $shinobi")
+  println(s"To JSON --> ${shinobi.asJson.noSpaces}")
+
 
 }
+
+
+
