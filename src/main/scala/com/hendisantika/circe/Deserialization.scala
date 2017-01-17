2c75b4031bf1d16289dc1b896d7a8b9ae4e7c5f8
hendisantika <hendisantika@gmail.com>
2017-01-17 18:59:01 +0700

    Bug Fix Deserialization

--- src/main/scala/com/hendisantika/circe/Deserialization.scala
+++ src/main/scala/com/hendisantika/circe/Deserialization.scala
@@ -1,8 +1,39 @@
 package com.hendisantika.circe
 
+import java.io.{FileInputStream, ObjectInputStream}
+
 /**
   * Created by hendisantika on 12/01/17.
   */
-class Deserialization {
 
+@SerialVersionUID(15L)
+class Animal(name: String, age: Int) extends Serializable {
+  override def toString = s"Animal($name, $age)"
+}
+
+case class Person(name: String)
+
+// or fork := true in sbt
+class ObjectInputStreamWithCustomClassLoader(
+                                              fileInputStream: FileInputStream
+                                            ) extends ObjectInputStream(fileInputStream) {
+  override def resolveClass(desc: java.io.ObjectStreamClass): Class[_] = {
+    try {
+      Class.forName(desc.getName, false, getClass.getClassLoader)
+    }
+    catch {
+      case ex: ClassNotFoundException => super.resolveClass(desc)
+    }
+  }
+}
+
+object MyDeserialize extends App {
+  val fis = new FileInputStream("../a.tmp")
+  val ois = new ObjectInputStreamWithCustomClassLoader(fis)
+
+  val animal = ois.readObject
+  val person = ois.readObject
+  ois.close
+  println(animal)
+  println(person)
 }
