1eef4d0f48f5dccf754c82e12dfe6cfdf81e61ca
hendisantika <hendisantika@gmail.com>
2017-01-17 18:58:38 +0700

    Bug Fix Marshal

--- src/main/scala/com/hendisantika/circe/Marshal.scala
+++ src/main/scala/com/hendisantika/circe/Marshal.scala
@@ -3,6 +3,36 @@ package com.hendisantika.circe
 /**
   * Created by hendisantika on 12/01/17.
   */
-object Marshal {
 
+import java.io._
+import scala.reflect.ClassManifest
+
+object Marshal extends App{
+
+  def dump[A](o: A)(implicit m: ClassManifest[A]): Array[Byte] = {
+    val ba = new ByteArrayOutputStream(512)
+    val out = new ObjectOutputStream(ba)
+    out.writeObject(m)
+    out.writeObject(o)
+    out.close()
+    ba.toByteArray()
+  }
+
+  @throws(classOf[IOException])
+  @throws(classOf[ClassCastException])
+  @throws(classOf[ClassNotFoundException])
+  def load[A](buffer: Array[Byte])(implicit expected: ClassManifest[A]): A = {
+    val in = new ObjectInputStream(new ByteArrayInputStream(buffer))
+    val found = in.readObject.asInstanceOf[ClassManifest[_]]
+    if (found <:< expected) {
+      val o = in.readObject.asInstanceOf[A]
+      in.close()
+      o
+    } else {
+      in.close()
+      throw new ClassCastException("type mismatch;" +
+        "\n found   : " + found +
+        "\n required: " + expected)
+    }
+  }
 }
